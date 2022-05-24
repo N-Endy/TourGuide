c20bdeca61609625fdeb1076c18f13d26ebfbdc9
Okafor Nelson <okafornel@gmail.com>
2022-05-24 12:17:36 +0100

    Add context with page title to PlacesAdapter

--- app/src/main/java/com/example/android/tourguide/PlacesAdapter.java
+++ app/src/main/java/com/example/android/tourguide/PlacesAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.tourguide;
 
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -12,14 +14,19 @@ import androidx.fragment.app.FragmentPagerAdapter;
  */
 public class PlacesAdapter extends FragmentPagerAdapter {
 
+    /** Context of the app */
+    private Context mContext;
+
     /**
      * Create a new {@link PlacesAdapter} object.
      *
      * @param fm is the fragment manager that will keep each fragment's state in the adapter
      *           across swipes.
+     * @param context is the context of the app
      */
-    public PlacesAdapter(@NonNull FragmentManager fm) {
+    public PlacesAdapter(Context context, @NonNull FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
     /**
@@ -46,5 +53,18 @@ public class PlacesAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_school);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_fast_food);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_hospital);
+        } else {
+            return mContext.getString(R.string.category_church);
+        }
     }
 }
