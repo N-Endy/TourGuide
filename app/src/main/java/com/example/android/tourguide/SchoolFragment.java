52613fbf8f24e287ccba1fe302893c63b573a78b
Okafor Nelson <okafornel@gmail.com>
2022-05-24 10:53:02 +0100

    Create SchoolFragment

---
+++ app/src/main/java/com/example/android/tourguide/SchoolFragment.java
@@ -0,0 +1,31 @@
+package com.example.android.tourguide;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * It displays a list of Schools in locality
+ */
+public class SchoolFragment extends Fragment {
+
+    public SchoolFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.place_list, container, false);
+
+
+
+        return rootView;
+    }
+}
